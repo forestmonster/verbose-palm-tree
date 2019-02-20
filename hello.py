a2e13545fb0e0b517f293afe863f899e004f7501
Forest Monsen <forest.monsen@gmail.com>
2019-02-20 13:46:38 -0800

    Use templates to render index and user.

--- hello.py
+++ hello.py
@@ -1,27 +1,13 @@
-from flask import Flask
-from flask import make_response
-from flask import redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello world!</h1>"
+    return render_template('index.html')
 
 
 @app.route("/user/<name>")
 def user(name):
-    return f"<h1>Hello {name}!</h1>", 401
-
-
-@app.route("/setcookie")
-def setCookie():
-    response = make_response("<h1>Setting a cookie.</h1>")
-    response.set_cookie("answer", "42")
-    return response
-
-
-@app.route("/send")
-def send():
-    return redirect("https://www.duckduckgo.com")
+    return render_template('user.html', name=name)
