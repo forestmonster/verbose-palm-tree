d29c3a49f3ee91bc4bc16e9ae8648e3cadd411ee
Forest Monsen <forest.monsen@gmail.com>
2019-02-22 11:37:44 -0800

    Add 404 and 500 error handlers.

--- hello.py
+++ hello.py
@@ -7,10 +7,20 @@ bootstrap = Bootstrap(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/user/", defaults={'name': 'Blah'})
+@app.route("/user/", defaults={"name": "Blah"})
 @app.route("/user/<name>")
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template("user.html", name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
