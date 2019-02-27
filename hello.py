8734d983b02250152bcd9b0e1334ad7dd59fc571
Forest Monsen <forest.monsen@gmail.com>
2019-02-27 12:17:03 -0800

    Start using WTForms. Create a NameForm class.

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
 from datetime import datetime
 from os import getenv
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-app.config['SECRET_KEY'] = getenv('FLASK_SECRET_KEY')
+app.config["SECRET_KEY"] = getenv("FLASK_SECRET_KEY")
 
 
 @app.route("/")
@@ -30,3 +33,8 @@ def page_not_found(error):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
+
+
+class Nameform(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
