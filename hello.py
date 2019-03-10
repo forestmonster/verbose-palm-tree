1d40d292fc9e1ea37c0260e0dcb962f0433bdbf5
Forest Monsen <forest.monsen@gmail.com>
2019-03-09 21:36:13 -0800

    Send "have we seen this user?" logic to the template.

--- hello.py
+++ hello.py
@@ -63,7 +63,11 @@ def index():
         form.name.data = ""
         return redirect(url_for("index"))
     return render_template(
-        "index.html", current_time=datetime.utcnow(), form=form, name=session.get("name")
+        "index.html",
+        current_time=datetime.utcnow(),
+        form=form,
+        name=session.get("name"),
+        known=session.get("known", False),
     )
 
 
