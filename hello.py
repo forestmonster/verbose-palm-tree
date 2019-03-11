c22cead5ef3d376762c68a9991d60d2a5ee6bda4
Forest Monsen <forest.monsen@gmail.com>
2019-03-11 16:59:52 -0700

    Set up to send mail.

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -16,10 +17,18 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
+app.config["MAIL_SERVER"] = "localhost"
+app.config["MAIL_PORT"] = 25
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["FLASKY_MAIL_SUBJECT_PREFIX"] = "[Flasky]"
+app.config["FLASKY_MAIL_SENDER"] = "Flasky Admin <flasky@example.com>"
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+mail = Mail(app)
 migrate = Migrate(app, db)
 
 
@@ -92,3 +101,14 @@ def page_not_found(error):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(
+        app.config["FLASKY_MAIL_SUBJECT_PREFIX"] + subject,
+        sender=app.config["FLASKY_MAIL_SENDER"],
+        recipients=[to],
+    )
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
