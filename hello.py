deb1f1ad7a69c0ef04009746a09b174107348ced
Forest Monsen <forest.monsen@gmail.com>
2019-03-10 20:09:21 -0700

    Use database migrations.

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -14,12 +15,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = getenv("FLASK_SECRET_KEY")
-
-db = SQLAlchemy(app)
 
 
 class NameForm(FlaskForm):
