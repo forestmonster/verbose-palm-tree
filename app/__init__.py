28a9ed30acd94172fce51ed2ae514df3fa425a5e
Forest Monsen <forest.monsen@gmail.com>
2019-03-15 17:12:38 -0700

    Initialize the app when requested.

--- app/__init__.py
+++ app/__init__.py
@@ -12,4 +12,12 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
-    pass
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    return app
