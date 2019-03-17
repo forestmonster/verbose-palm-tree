78fe8e20abdd5c729e9b12744437edfccd13027a
Forest Monsen <forest.monsen@gmail.com>
2019-03-16 18:46:13 -0700

    We moved all this stuff out of here.

--- hello.py
+++ hello.py
@@ -1,19 +1,3 @@
-import os
-import logging
-from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_mail import Mail
-from flask_migrate import Migrate
-from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from threading import Thread
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
 app.config["MAIL_SERVER"] = "localhost"
@@ -24,44 +8,3 @@ app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
 app.config["FLASKY_ADMIN"] = os.environ.get("FLASKY_ADMIN")
 app.config["FLASKY_MAIL_SUBJECT_PREFIX"] = "[Flasky]"
 app.config["FLASKY_MAIL_SENDER"] = "Flasky Admin <flasky@example.com>"
-
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-db = SQLAlchemy(app)
-mail = Mail(app)
-migrate = Migrate(app, db)
-
-
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
-    favorite_color = db.Column(db.String(64), index=True, nullable=True)
-
-    def __repr__(self):
-        return "<User %r>" % self.username
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
-
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
-
-
-def send_email(to, subject, template, **kwargs):
-    msg = Message(
-        app.config["FLASKY_MAIL_SUBJECT_PREFIX"] + subject,
-        sender=app.config["FLASKY_MAIL_SENDER"],
-        recipients=[to],
-    )
-    msg.body = render_template(template + ".txt", **kwargs)
-    msg.html = render_template(template + ".html", **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    app.logger.info("Sending mail")
-    mail.send(msg)
