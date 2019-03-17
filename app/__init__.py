f937e950924ae97f4b3b93d33cdd13f82d117074
Forest Monsen <forest.monsen@gmail.com>
2019-03-16 18:43:16 -0700

    Instantiate the app.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
@@ -12,6 +12,7 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
+    app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
