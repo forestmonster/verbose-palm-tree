c3486af6d74b876b2013521a30e9feca3cb7dd3a
Forest Monsen <forest.monsen@gmail.com>
2019-03-21 13:31:48 -0700

    Add password setter and verification methods.

--- app/models.py
+++ app/models.py
@@ -17,6 +17,13 @@ class User(db.Model):
     def password(self):
         raise AttributeError("Password is not a readable attribute.")
 
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Role(db.Model):
     __tablename__ = "roles"
