37a357d5fb50b973ff55a919b895f64680363802
Forest Monsen <forest.monsen@gmail.com>
2019-03-21 13:52:18 -0700

    Test the password setter method.

---
+++ tests/test_user_model.py
@@ -0,0 +1,8 @@
+import unittest
+from app.models import User
+
+
+class UserModelTestCase(unittest.TestCase):
+    def test_password_setter(self):
+        u = User(password="cat")
+        self.assertTrue(u.password_hash is not None)
