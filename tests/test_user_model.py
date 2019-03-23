91ec91bd4aa5074c905d2138b97c8add235b3511
Forest Monsen <forest.monsen@gmail.com>
2019-03-22 22:04:59 -0700

    Test to ensure we can't read the pw attribute.

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -6,3 +6,8 @@ class UserModelTestCase(unittest.TestCase):
     def test_password_setter(self):
         u = User(password="cat")
         self.assertTrue(u.password_hash is not None)
+
+    def test_no_password_getter(self):
+        u = User(password="cat")
+        with self.assertRaises(AttributeError):
+            u.password
