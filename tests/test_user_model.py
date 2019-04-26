111162d7c1aab50a4a970521e8251e20bd2740d1
Forest Monsen <forest.monsen@gmail.com>
2019-04-26 11:44:41 -0700

    Add test for invalid reset tokens.

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -59,3 +59,11 @@ class UserModelTestCase(unittest.TestCase):
         token = u.generate_reset_token()
         self.assertTrue(User.reset_password(token, "dog"))
         self.assertTrue(u.verify_password("dog"))
+
+    def test_invalid_reset_token(self):
+        u = User(password="cat")
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_reset_token()
+        self.assertFalse(User.reset_password(token + "a", "horse"))
+        self.assertTrue(u.verify_password("cat"))
