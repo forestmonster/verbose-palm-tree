e952b5026bff6c05a74efaba0670e4421b637628
Forest Monsen <forest.monsen@gmail.com>
2019-04-30 10:36:37 -0700

    We're going to use these forms.

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,14 @@ from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import db, User
-from .forms import LoginForm, RegistrationForm, PasswordUpdateForm
+from .forms import (
+    ChangePasswordForm,
+    LoginForm,
+    PasswordResetForm,
+    PasswordResetRequestForm,
+    PasswordUpdateForm,
+    RegistrationForm,
+)
 from ..email import send_email
 
 
