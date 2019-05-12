314327a78c746cd67785cdbbb46bf3193f68192d
Forest Monsen <forest.monsen@gmail.com>
2019-05-12 11:04:14 -0700

    Remove comment.

--- config.py
+++ config.py
@@ -42,7 +42,6 @@ config = {
     "development": DevelopmentConfig,
     "testing": TestingConfig,
     "production": ProductionConfig,
-    # Development is fine.
     "default": DevelopmentConfig,
 }
 
